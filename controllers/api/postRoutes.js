a6d0b92aea7a27ab6bf995ee5863fa80b3655c12
Caitlin Bopp <boppcaitlin@gmail.com>
2021-06-12 09:48:49 -0400

    Updated create function

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -8,6 +8,7 @@ router.post('/', withAuth, async (req, res) => {
             ...req.body,
             user_id: req.session.user_id,
         });
+        console.log(req.body);
 
         res.status(200).json(newPost);
     }   catch (err) {
--- public/js/posts.js
+++ public/js/posts.js
@@ -4,10 +4,13 @@ const addPost = async (event) => {
     const title = document.querySelector('#post-title').value.trim();
     const content = document.querySelector('#post-content').value.trim();
 
+    console.log(title);
+    console.log(content);
+
     if (title && content) {
         const response = await fetch('/api/posts', {
             method: 'POST',
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({ "title": title, "content": content }),
             headers: { 'Content-Type': 'application/json' },
         });
 
